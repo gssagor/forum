48841c41297bf63619ad827aeed829b7da66e86a
gssagor <gssagor635@gmail.com>
2020-03-13 21:38:58 +0600

    clueless commit

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,4 +1,4 @@
-<div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom box-shadow">
+<div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom box-shadow" id="menu">
     <h5 class="my-0 mr-md-auto font-weight-normal"><a style="text-decoration:none;" href="/">StuckInAI</a></h5>
     <nav class="my-2 my-md-0 mr-md-3">
         <form role="form" id="form-buscar">
